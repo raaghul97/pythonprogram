4a1787822afa86182635d17fe4f5f4db24c37153
raaghul97 <37371873+raaghul97@users.noreply.github.com>
2018-04-30 10:10:46 +0530

    Update number.py

--- beginner level/number.py
+++ beginner level/number.py
@@ -1,7 +1,8 @@
 x=int(input("enter a number"))
-if (x==0):
-  print ("The number is zero")
-if (x>0):
-  print("The number is positive ")
-if(x<0):
-  print("The number is negative")
+if (x>0 and x<=10000):
+  if (x==0):
+    print ("The number is zero")
+  if (x>0):
+    print("The number is positive ")
+  if(x<0):
+    print("The number is negative")
